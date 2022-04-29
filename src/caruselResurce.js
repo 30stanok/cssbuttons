29deabae3952b7b0078ee73d71560250dd8b6ce8
poiq <bloodbornps@mail.ru>
2022-04-29 14:41:14 +0700

    accept

--- src/caruselResurce.js
+++ src/caruselResurce.js
@@ -1,3 +1,25 @@
-const Nikita
-const Artem
-const
+const userForm  = {
+    photo : "user1.jped",
+    text : "mytext",
+}
+
+const peopleInTeam = 7;
+const ul = document.getElementById(`ul`);
+
+for (let i = 0; i < peopleInTeam; i++) {
+    const scrollblock = document.createElement(`li`);
+    scrollblock.id = `user${i}`;
+    scrollblock.className = `scroll`;
+    const userimage = document.createElement(`img`);
+    userimage.id = `userimg${i}`;
+    userimage.className = `photo`;
+    userimage.src = `#`;    //../materials/caruselMaterials/user${i}.jpeg
+    userimage.alt = `photo`;
+    const usertext = document.createElement(`div`);
+    usertext.id = `usertext${i}`;
+    usertext.className = `text`
+    usertext.innerHTML = `hello`;
+    scrollblock.appendChild(userimage);
+    scrollblock.appendChild(usertext);
+    ul.appendChild(scrollblock);
+}
